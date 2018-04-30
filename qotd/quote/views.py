480c06df2560f600a8325ad989404f1222157c9d
Yogen Rai <yogen.rai.992@gmail.com>
2018-04-29 18:42:52 -0700

    Added random quote display functionality.

--- qotd/quote/views.py
+++ qotd/quote/views.py
@@ -8,5 +8,4 @@ from quote.models import Quote
 def quote(request):
     quotes = Quote.objects.all()
     qotd = quotes[randint(0, len(quotes)-1)]
-    # qotd = Quote(content="Legend are made, not born!", author="Yogen Rai", dateCreated=today)
     return render(request, "quote.html", {"quote": qotd})
